b7dc20dfecbbfa263d38f5c4968b781a636a488a
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-09-01 19:29:14 -0600

    Create entry points

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +20,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    UsersModule,
+    ProductsModule,
+    SalesModule,
+    InventoryModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
